ffb30f420ef82e8d955270edc7f5cc5e2ea47fae
Manuel Portmann <manuel.portmann@stud.hslu.ch>
2024-11-30 15:39:07 +0900

    add highscore

--- game.py
+++ game.py
@@ -15,7 +15,7 @@ class Game:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        pygame.display.set_caption("Flappy Bird")
+        pygame.display.set_caption("FlappyPy")
         self.clock = pygame.time.Clock()
         self.bird = Bird()
         self.pipes = []
